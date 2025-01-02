015ea69f6be7ed5c1f5aae26c6a5992232bdad16
Ali George <george7ali@gmail.com>
2025-01-02 23:14:48 +0100

    import the card component and use to render the card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,12 @@
 import React from "react";
+import Card from "./CArd";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2 className="name">Beyonce</h2>
-          <img className="circle-img"
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p className="info">+123 456 789</p>
-          <p className="info">b@beyonce.com</p>
-        </div>
-      </div>
+      <Card name="Wagon Masonry" img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+    alt="avatar_img" phone="+5362-76378-78373" email="el-bona94@yahoo.com" />
     </div>
   );
 }
