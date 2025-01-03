5e570863250fcbaabf367d2b81f26751a07f3674
Ali George <george7ali@gmail.com>
2025-01-03 11:38:23 +0100

    use props to render the contact card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
-function Card(){
+function Card(props){
     return (
         <div className="card">
-<div className="top">
-  <h2 className="name">Wagon Masonry</h2>
-  <img className="circle-img"
-    src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-    alt="avatar_img"
-  />
-</div>
-<div className="bottom">
-  <p className="info">+123 456 789</p>
-  <p className="info">b@beyonce.com</p>
-</div>
-</div>
+        <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <img className="circle-img"
+            src={props.img}
+            alt="avatar_img"
+        />
+            </div>
+        <div className="bottom">
+        <p className="info">{props.phone}</p>
+        <p className="info">{props.email}</p>
+        </div>
+        </div>
     )
 }
 
